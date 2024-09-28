2c3a2da8ac8dfa454f60147a8cbef5554572f1bc
jan <jan.koskinen94@gmail.com>
2024-09-28 20:00:30 +0300

    Exercise 1.02

---
+++ project/todo-server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return defaultValue
+	}
+	return value
+}
+
+var PORT = getEnv("PORT", "8080")
+
+func main() {
+	port := ":" + PORT
+	fmt.Printf("Server listening on port %s\n", port)
+	http.ListenAndServe(port, nil)
+}
